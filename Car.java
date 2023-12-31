07bedbba647a2127dcb9fcea68bf2c2e37afacf7
om4rinku <44322332+om4rinku@users.noreply.github.com>
2023-12-31 11:14:25 +0530

    Update Car.java
    removed

--- Car.java
+++ Car.java
@@ -30,13 +30,6 @@ class Car extends Vehicle {
                 " wheels, classified as a " + type + ", is currently in gear " + currentGear + ".");
     }
 
-
-
-//    public void changeGear(int newGear) {
-//        this.currentGear = newGear;
-//        System.out.println("A " + getName() + "SUV equipped with"+ wheels + " wheels, classified as a "+type+ " is currently moving at a speed of "+++ currentGear);
-//    } //A Tata SUV equipped with 4 wheels, classified as an SUV, is currently moving at a speed of 12 km/h and is in gear 2
-
     public void changeSpeed(double speed, String direction) {
         move(speed, direction);
     }
