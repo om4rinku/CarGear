b2f2e0d954820421992bc8e3804963add9e0f414
om4rinku <44322332+om4rinku@users.noreply.github.com>
2023-12-31 11:18:31 +0530

    Update SUV.java
    changes done

--- SUV.java
+++ SUV.java
@@ -8,12 +8,9 @@ class SUV extends Car {
             System.out.println("Speed cannot be less than 0.");
             return;
         }
-
-        // Accessing currentSpeed using the getter method
         double updatedSpeed = getCurrentSpeed() + newSpeed;
 
         if (newSpeed == 0) {
-            // Accessing currentGear using the setter method
             changeGear(0);
         } else if (newSpeed <= 20) {
             changeGear(1);
@@ -24,8 +21,5 @@ class SUV extends Car {
         } else if (newSpeed <= 80) {
             changeGear(4);
         }
-
-
-       // System.out.println(getName() + " accelerated to speed " + updatedSpeed + " in gear " + getCurrentGear());
     }
 }
