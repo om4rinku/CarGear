a1d39404027e3c2312a106477a557b4d5671f3ad
om4rinku <44322332+om4rinku@users.noreply.github.com>
2023-12-31 11:22:31 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,44 +1,27 @@
-// Abstract class representing a generic vehicle
 abstract class Vehicle {
-    // Private attributes of a vehicle
-    private String name;                // Name of the vehicle
-    private double currentSpeed;        // Current speed of the vehicle
-    private String currentDirection;    // Current direction of the vehicle
-
-    // Constructor to initialize the vehicle with a name and default values
+    private String name;
+    private double currentSpeed;
+    private String currentDirection;
     public Vehicle(String name) {
         this.name = name;
-        this.currentSpeed = 0;           // Default speed is set to 0
-        this.currentDirection = "N/A";   // Default direction is set to "N/A"
+        this.currentSpeed = 0;
+        this.currentDirection = "N/A";
     }
-
-    // Abstract method for moving the vehicle, to be implemented by subclasses
     public abstract void move(double speed, String direction);
-
-    // Abstract method for steering the vehicle, to be implemented by subclasses
     public abstract void steer(String direction);
-
-    // Getter method to retrieve the name of the vehicle
+    
     public String getName() {
         return name;
     }
-
-    // Getter method to retrieve the current speed of the vehicle
     public double getCurrentSpeed() {
         return currentSpeed;
     }
-
-     //Setter method to update the current speed of the vehicle
     public void setCurrentSpeed(double currentSpeed) {
         this.currentSpeed = currentSpeed;
     }
-
-    // Getter method to retrieve the current direction of the vehicle
     public String getCurrentDirection() {
         return currentDirection;
     }
-
-    // Setter method to update the current direction of the vehicle
     public void setCurrentDirection(String currentDirection) {
         this.currentDirection = currentDirection;
     }
